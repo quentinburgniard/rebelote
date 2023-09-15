3ab2e16a8229211a739ae4e0f033e1b6e6e6ec42
Quentin Burgniard <quentinburgniard@gmail.com>
2023-09-16 00:50:43 +0200

    fix(dependencies)

---
+++ fr.js
@@ -0,0 +1,14 @@
+export default {
+  'Change password': 'Modifier mot de passe',
+  'Confirm password': 'Confirmer mot de passe',
+  'Current password': 'Mot de passe actuel',
+  'Email': 'Email',
+  'Forgot password': 'Mot de passe oublié',
+  'Home page': "Page d'accueil",
+  'Icon Digital Léman': '',
+  'New password': 'Nouveau mot de passe',
+  'Password': 'Mot de passe',
+  'Reset password': 'Réinitialiser mot de passe',
+  'Sign In': 'Se connecter',
+  'Sign Out': 'Se déconnecter'
+}
---
+++ pt.js
@@ -0,0 +1,7 @@
+export default {
+  'Email': 'E-mail',
+  'Forgot password': 'Esqueci a senha',
+  'Password': 'Senha',
+  'Reset password': 'Redefinir senha',
+  'Sign In': 'Entrar'
+}
