7dd612f7f90d6cc144667067fb9d67632e477dc4
Quentin Burgniard <quentinburgniard@gmail.com>
2023-09-16 13:13:25 +0200

    fix(routine-execution)

--- public/routine-execution.js
+++ public/routine-execution.js
@@ -13,7 +13,7 @@ const Step = {
         method: 'post',
         body: JSON.stringify({
           data: {
-            execution: this.$root.id,
+            execution: executionID,
             step: this.step.id,
             value: this.value,
           }
@@ -41,38 +41,45 @@ const app = createApp({
   },
   data() {
     return {
-      id: id,
-      step: 0,
+      step: null,
       steps: steps
     }
   },
   methods: {
     nextStep() {
-      if (window.location.hash == '#0') {
-        window.location.hash = `#${this.currentStep ? this.currentStep.id : 0}`;
-      } else {
-        fetch(`https://api.digitalleman.com/v2/routine-executions/${this.id}?populate[0]=stepExecutions&populate[1]=stepExecutions.step`, {
-          headers: {
-            'authorization': `Bearer ${ID.getToken()}`,
-            'content-type': 'application/json'
-          }
-        }).then((response) => response.json()).then((response) => {
-          response.data.attributes.stepExecutions.data.forEach((stepExecution) => {
-            let step = this.steps.find((step) => step.id == stepExecution.attributes.step.data.id);
-            if (step) step.execution = stepExecution;
-          });
-          window.location.hash = `#${this.currentStep ? this.currentStep.id : 0}`;
-        }).catch(error => {
-          console.log(error);
-        }); 
-      }
+      fetch(`https://api.digitalleman.com/v2/routine-executions/${executionID}?populate[0]=stepExecutions&populate[1]=stepExecutions.step`, {
+        headers: {
+          'authorization': `Bearer ${ID.getToken()}`,
+          'content-type': 'application/json'
+        }
+      }).then((response) => response.json()).then((response) => {
+        response.data.attributes.stepExecutions.data.forEach((stepExecution) => {
+          let step = this.steps.find((step) => step.id == stepExecution.attributes.step.data.id);
+          if (step) step.execution = stepExecution;
+        });
+        if (this.currentStep) {
+          window.location.hash = `#${this.currentStep.id}`;
+        } else {
+          window.location.replace(`https://rebelote.digitalleman.com/fr/${routineID}`);
+        }
+      }).catch(error => {
+        console.log(error);
+      });
     },
     getStep() {
       let hash = window.location.hash.slice(1);
       let index = this.steps.findIndex((step) => step.id == hash);
-      let step = 0;
-      if (index != -1) step = this.steps.find((step, stepIndex) => !step.execution || stepIndex == index).id;
-      if (window.location.hash != `#${step}`) window.location.hash = `#${step}`;
+      let step = null;
+      if (index != -1) {
+        step = this.steps.find((step, stepIndex) => !step.execution || stepIndex == index).id;
+      } else if (this.currentStep) {
+        step = this.currentStep.id;
+      }
+      if (step) {
+        if (window.location.hash != `#${step}`) window.location.hash = `#${step}`;
+      } else {
+        window.location.replace(`https://rebelote.digitalleman.com/fr/${routineID}`);
+      }
       return step;
     }
   },
